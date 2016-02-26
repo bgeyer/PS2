099530a1de7cd819570376513263c39d3bfd3a0d
Bonard Geyer <bgeyer@udel.edu>
2016-02-26 13:43:57 -0500

    final

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -30,5 +30,37 @@ public class MyInteger_Test {
 	public void test() {
 		assertTrue(1==1);
 	}
+	@Test
+	public void testisEven() {
+		int iIntegerTestValue = 8;
+		MyInteger instance = new MyInteger(iIntegerTestValue);
+		boolean bExpectedresult = true;
+		boolean bActualResult = instance.isEven();
+		assertEquals("testisEven() failed", bExpectedresult,bActualResult);
+	}
+		@Test
+		public void testisOdd() {
+			int iIntegerTestValue = 9;
+			MyInteger instance = new MyInteger(iIntegerTestValue);
+			boolean bExpectedresult = true;
+			boolean bActualResult = instance.isOdd();
+			assertEquals("testisEven() failed", bExpectedresult,bActualResult);
+	}
+		@Test
+		public void testisPrime() {
+			int iIntegerTestValue = 17;
+			MyInteger instance = new MyInteger(iIntegerTestValue);
+			boolean bExpectedresult = true;
+			boolean bActualResult = instance.isPrime();
+			assertEquals("testisEven() failed", bExpectedresult,bActualResult);
 
+}
+		@Test
+		public void testequals() {
+			int iIntegerTestValue = 8;
+			MyInteger instance = new MyInteger(iIntegerTestValue);
+			boolean bExpectedresult = true;
+			boolean bActualResult = instance.equals(8);
+			assertEquals("testisEven() failed", bExpectedresult,bActualResult);
+}
 }
